e72896c58833d18a562f36335c150524293d4d99
JiDong <jidong_cs@163.com>
2019-07-01 23:01:16 +0800

    Update index.js

--- js/qa/index.js
+++ js/qa/index.js
@@ -17,7 +17,7 @@ Vue.component("app-header", {
         <div class="nav">
           <a href="" class="selected" title="首页">首页</a>
           <a href="" title="消息">消息</a>
-          <a href="" v-bind:title="user.name"><img v-bind:src="user.avatar_url" class="avatar"/></a>
+          <a href="" v-bind:title="user.name"><img v-bind:src="user.avatar" class="avatar"/></a>
         </div>
       </div>
     </div>
@@ -41,7 +41,7 @@ Vue.component("question-card", {
         </div>
         <div class="answer-author">
           <a>
-            <img v-bind:src="question.answer.author.avatar_url">{{question.answer.author.name}}
+            <img v-bind:src="question.answer.author.avatar">{{question.answer.author.name}}
           </a>
           &nbsp;|&nbsp;
           <span>{{question.answer.author.desc}}</span>
@@ -85,11 +85,12 @@ Vue.component("hot-question-item", {
       </div>
       <div class="answers">
         <div class="answer-author">
-          <a>
-            <img v-bind:src="question.answer.author.avatar_url">{{question.answer.author.name}}
+          <a href="">
+            <img v-bind:src="question.answer.author.avatar">
+            <span class="name">{{question.answer.author.name}}</span>
+            &nbsp;|&nbsp;
+            <span class="desc">{{question.answer.author.desc}}</span>
           </a>
-          &nbsp;|&nbsp;
-          <span>{{question.answer.author.desc}}</span>
         </div>
         <div v-if="question.answer.pictures.length > 0" class="answer-pic">
           <img v-bind:src="question.answer.pictures[0]">
@@ -108,17 +109,22 @@ Vue.component("user-card", {
     <div class="card user-card">
       <div>
         <a v-bind:href="'./user.html?uid=' + user.id">
-          <img v-bind:src="user.avatar_url" class="avatar">
-          <span class="username">{{user.name}}</span>
+          <div class="avatar">
+            <img v-bind:src="user.avatar">
+          </div>
+          <div class="user-info">
+            <div class="name">{{user.name}}</div>
+            <div class="desc">{{user.desc}}</div>
+          </div>
         </a>
       </div>
       <div>
         <ul>
-          <li><a>我的回答{{user.num_answer}}</a></li>
-          <li><a>我的收藏{{user.num_collect}}</a></li>
-          <li><a>我的邀约{{user.num_invite}}</a></li>
-          <li><a>我的关注{{user.num_follow}}</a></li>
-          <li><a>我的粉丝{{user.num_fans}}</a></li>
+          <li><a href="">我的回答{{user.num_answer}}</a></li>
+          <li><a href="">我的收藏{{user.num_collect}}</a></li>
+          <li><a href="">我的邀约{{user.num_invite}}</a></li>
+          <li><a href="">我的关注{{user.num_follow}}</a></li>
+          <li><a href="">我的粉丝{{user.num_fans}}</a></li>
         </ul>
       </div>
     </div>
@@ -129,17 +135,21 @@ Vue.component("creator-card", {
   props: ['user'],
   template: `
     <div class="card creator-card">
-      <div>
-        <a v-bind:href="'./user.html?uid=' + user.id">创作者中心</a>
-      </div>
-      <div>
-        <a>
-          <span>昨日回答阅读数</span><span>{{user.num_read}}</span>
-        </a>
-        <a>
-          <span>昨日回答赞同数</span><span>{{user.num_agree}}</span>
-        </a>
-      </div>
+      <a v-bind:href="'./user.html?uid=' + user.id">
+        <div class="creator-entrance-text">
+          创作者中心
+        </div>
+        <div class="analytics">
+          <div>
+            <div>昨日回答阅读数</div>
+            <div>{{user.num_read}}</div>
+          </div>
+          <div>
+            <div>昨日回答赞同数</div>
+            <div>{{user.num_agree}}</div>
+          </div>
+        </div>
+      </a>
     </div>
   `
 })
@@ -157,6 +167,20 @@ Vue.component("copyright-card", {
   
 })
 
+user = {
+      id: 1233342442,
+      name: "季冬", 
+      desc: "东南大学计算机专业研究生",
+      avatar: "http://p9-tt.byteimg.com/large/pgc-image/3e675927d59a4257967100ac34df2a3f",
+      num_answer: 10,
+      num_collect: 100,
+      num_follow: 45,
+      num_fans: 300,
+      num_invite: 12,
+      num_read: 1000,
+      num_agree: 300
+    };
+    
 questionList = []
 for(var i=1; i< 15; i++) {
   questionList.push({
@@ -165,7 +189,7 @@ for(var i=1; i< 15; i++) {
     answer_num: 45,
     collect_num: 58,
     answer:{
-      author:{name: "季冬", avatar_url: "", desc: "东南大学计算机专业研究生"}, 
+      author: user, 
       abstract: "特朗普在要去日本参加G20峰会前，放出话想退出“美日安保条约”，可是没看到日本民众反对，日本政府怕美国退出是真的。为什么？特朗普认为美日安保条约对美国不公平：在日本受到攻击时，条约",
       pictures: ["http://n.sinaimg.cn/finance/crawl/595/w550h845/20190629/ad6c-hyzpvis2535634.jpg"],
       like_num: 2000,
@@ -183,7 +207,7 @@ for(var i=1; i< 5; i++) {
     answer_num: 45,
     collect_num: 58,
     answer:{
-      author:{name: "季冬", avatar_url: "", desc: "东南大学计算机专业研究生"}, 
+      author:user, 
       abstract: "特朗普在要去日本参加G20峰会前，放出话想退出“美日安保条约”，可是没看到日本民众反对，日本政府怕美国退出是真的。为什么？特朗普认为美日安保条约对美国不公平：在日本受到攻击时，条约",
       pictures: ["http://n.sinaimg.cn/finance/crawl/595/w550h845/20190629/ad6c-hyzpvis2535634.jpg"],
       like_num: 2000,
@@ -201,17 +225,6 @@ new Vue({
     app: {
       title: '首页 - 知否', logo: 'https://s3.pstatp.com/toutiao/resource/wenda/wenda_web/static/style/image/logo_5df43ca.png'
     },
-    user: {
-      id: 1233342442,
-      name: "季冬", 
-      avatar_url: "http://p9-tt.byteimg.com/large/pgc-image/3e675927d59a4257967100ac34df2a3f",
-      num_answer: 10,
-      num_collect: 100,
-      num_follow: 45,
-      num_fans: 300,
-      num_invite: 12,
-      num_read: 1000,
-      num_agree: 300
-    }
+    user: user
   }
 });
